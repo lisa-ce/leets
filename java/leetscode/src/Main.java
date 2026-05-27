75578426c1cec23d07158cd696331efae27eddb0
Lisa Chikovore <lisachikovore@gmail.com>
2026-05-28 00:01:43 +0200

    added leet 2

--- java/leetscode/src/Main.java
+++ java/leetscode/src/Main.java
@@ -31,6 +31,40 @@
 
 
 //leet 2
+//
+//public class Main {
+//    public static void main(String[] args) {
+//int n=10;
+//int count=0;
+//
+//
+//for(int i=1;i<=n;i++){
+//    int num=i;
+//    boolean changed=false;
+//    boolean valid=true;
+//    while(num>0){
+//        int digit=num%10;
+//
+//        if(digit==3 || digit==4 || digit==7){
+//            valid=false;
+//            break;
+//
+//        }
+//        if(digit==2 || digit==6 || digit==9){
+//            changed=true;
+//        }
+//        num=num/10;
+//    }
+//    if(valid && changed){
+//        count++;
+//    }
+//        System.out.println(count);
+//}
+//    }
+
+
+//leet 3
+
 
 
 
